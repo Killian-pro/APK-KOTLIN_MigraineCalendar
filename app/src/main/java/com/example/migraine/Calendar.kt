3ce38238a68f6fa3f34bcd1dd208ee6b256521ed
kip <cluzel.killian@gmail.com>
2020-08-28 14:59:16 +0200

    Working_APP

---
+++ app/src/main/java/com/example/migraine/Calendar.kt
@@ -0,0 +1,74 @@
+package com.example.migraine
+
+import android.content.Context
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Integer.parseInt
+import java.util.regex.Pattern
+
+class Calendar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+        onload()
+        afficher("A")
+    }
+
+    fun onload() {
+        val YEAR = findViewById(R.id.YEAR) as EditText
+
+        //RECUPERE LA VALEUR DE YEAR
+        var Mon_Annee_choisi = "2020"
+
+        //RECUPERE LA VALEUR DE YEAR
+        YEAR.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                Mon_Annee_choisi = s.toString()
+                afficher(Mon_Annee_choisi)
+            }
+        })
+
+
+    }
+
+    fun afficher(Mon_Annee_choisi: String) {
+
+        //ancienne valeur
+        var valeur_final =
+            "\n25-08-2020\n22-08-2020\n20-07-2020\n15-07-2020\n01-07-2020\n23-06-2020\n14-06-2020\n07-06-2020\n10-05-2020\n24-04-2020\n03-04-2020\n01-03-2020\n28-02-2020\n23-02-2020\n05-02-2020\n09-01-2020\n03-01-2020\n25-12-2019\n18-12-2019\n16-12-2019\n13-12-2019\n07-12-2019\n08-12-2019\n25-11-2019\n19-11-2019\n15-11-2019\n16-11-2019\n06-10-2019\n12-09-2019\n13-09-2019\n18-09-2019\n19-09-2019\n22-09-2019\n28-09-2019"
+
+        val EditText = findViewById(R.id.MULTI) as EditText
+        val sharedPref = getSharedPreferences("KIKI", Context.MODE_PRIVATE) ?: return
+        sharedPref.edit()
+        val init = sharedPref.getString("push", "")
+        val nombre_val = parseInt(init)
+
+        for (val_increment in 0..nombre_val) {
+            val valeur = sharedPref.getString(val_increment.toString(), "")
+            if (valeur != null) {
+                //  Log.v("NB VAL", valeur)
+
+                valeur_final = valeur + "\n" + valeur_final
+
+                //choisir tout sauf l'année ecrit >> ne marche pas
+                //val regex = Regex("^((?!$Mon_Annee_choisi).)*$")
+                val regex = Regex("......$Mon_Annee_choisi")
+                val regex2 = Regex("(?m)^[ \\t]*\\r?\\n")
+                valeur_final = valeur_final.replace(regex, "")
+                //enlever tout les sauts de ligne
+                valeur_final=valeur_final.replace(regex2,"")
+                EditText.setText(valeur_final.trim());
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/migraine/MainActivity.kt
@@ -0,0 +1,161 @@
+package com.example.migraine
+
+
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Integer.parseInt
+import java.time.LocalDateTime
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        time();
+        affichage();
+
+    }
+
+    fun time() {
+        val register = findViewById(R.id.register) as Button
+        //recuperation du temps actuelle
+        val timeactual = System.currentTimeMillis();
+
+        //recuperation de l'ancienne valeur enregistré
+        var prefs: SharedPreferences? = null
+        prefs = this.getSharedPreferences("KIKI", 0)
+
+        val OLD = prefs!!.getLong("time", 0)
+        var init = prefs!!.getString("push", null)
+        // difference en MILLISECOND
+        val Diff = timeactual - OLD
+        // DIFF EN HEURE
+        var hours = (Diff / (1000 * 60 * 60))
+
+        register.setOnClickListener {
+            // METTRE <2
+            if (hours < 2) {
+                showDialog();
+            } else {
+
+                var pre_init = parseInt(init);
+                pre_init++
+                init = pre_init.toString();
+                val editor = prefs!!.edit()
+                editor.putLong("time", System.currentTimeMillis());
+                editor.putString("push", init);
+                val mavalue = prefs!!.getString("push", "")
+                Log.v("VALUE", mavalue!!)
+                // SAUVEGARDE DE LA DATE
+                val now = LocalDateTime.now();
+                var year = now.year.toString()
+                var month = "0" + now.monthValue.toString()
+                var day = "0" + now.dayOfMonth.toString()
+                //ternaire IF LA DATE JOLIE
+                if (day.length > 2) day = day.substring(1)
+                if (month.length > 2) month = month.substring(1)
+                val String_Date = "$day-$month-$year"
+                Log.v("time", String_Date.toString()!!)
+                editor.putString(init, String_Date.toString());
+                editor.apply()
+                affichage()
+            }
+
+        }
+
+    }
+
+
+    fun affichage() {
+
+        val textView = findViewById(R.id.textView) as TextView
+        var prefs: SharedPreferences? = null
+        //recuperation du temps actuelle
+        val timeactual = System.currentTimeMillis();
+
+        //recuperation de l'ancienne valeur enregistré
+        prefs = this.getSharedPreferences("KIKI", 0)
+
+        val OLD = prefs!!.getLong("time", 0)
+
+
+        // difference en MILLISECOND
+        val Diff = timeactual - OLD
+
+        // EN HEURE ET EN MINUTES
+        var affichage = ""
+        var hours = (Diff / (1000 * 60 * 60)).toString() + " Heures";
+        var minutes = (Diff / (1000 * 60)).toString() + " Minutes";
+        var day = (Diff / (1000 * 60 * 60 * 24)).toString() + " Jours";
+
+        if ((Diff / (1000 * 60)) < 60) {
+            affichage = minutes
+        } else if ((Diff / (1000 * 60 * 60)) < 24) {
+            affichage = hours
+        } else {
+            affichage = day
+        }
+        textView.setText(affichage);
+
+
+    }
+
+
+    fun showDialog() {
+        // Late initialize an alert dialog object
+        lateinit var dialog: AlertDialog
+
+
+        // Initialize a new instance of alert dialog builder object
+        val builder = AlertDialog.Builder(this)
+
+        // Set a title for alert dialog
+        builder.setTitle("Tu as pris le medicament il y a moins de 2H")
+
+        // Set a message for alert dialog
+        //builder.setMessage("This is a sample message of AlertDialog.")
+
+
+        // On click listener for dialog buttons
+        val dialogClickListener = DialogInterface.OnClickListener { _, which ->
+            when (which) {
+                DialogInterface.BUTTON_POSITIVE -> toast("Non recommandé")
+            }
+        }
+
+
+        // Set the alert dialog positive/yes button
+        builder.setPositiveButton("OK", dialogClickListener)
+
+
+        // Initialize the AlertDialog using builder object
+        dialog = builder.create()
+
+        // Finally, display the alert dialog
+        dialog.show()
+    }
+
+
+    // Extension function to show toast message
+    fun Context.toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun OPENCALENDAR(view: View) {
+        val Calendar = Intent(this, Calendar::class.java)
+        // Do something in response to button
+        startActivity(Calendar)
+        //finish();
+    }
+
+}
